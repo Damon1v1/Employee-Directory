adc6290b282af5ad0b5bc7dfba1333486453f68c
Damon Burda <damonburda96@gmail.com>
2021-04-28 14:05:35 -0700

    component update

--- src/components/DataArea.js
+++ src/components/DataArea.js
@@ -32,26 +32,22 @@ export default class DataArea extends Component {
 
         const compareFnc = (a, b) => {
             if (this.state.order === "ascend") {
-              // account for missing values
               if (a[heading] === undefined) {
                 return 1;
               } else if (b[heading] === undefined) {
                 return -1;
               }
-              // numerically
               else if (heading === "name") {
                 return a[heading].first.localeCompare(b[heading].first);
               } else {
                 return a[heading] - b[heading];
               }
             } else {
-              // account for missing values
               if (a[heading] === undefined) {
                 return 1;
               } else if (b[heading] === undefined) {
                 return -1;
               }
-              // numerically
               else if (heading === "name") {
                 return b[heading].first.localeCompare(a[heading].first);
               } else {
